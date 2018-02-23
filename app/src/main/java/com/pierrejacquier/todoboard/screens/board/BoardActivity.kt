ebc9ceea95268c8a530ff897cc57206aca8f7c6f
Pierre Jacquier <pierrejacquier39@gmail.com>
2018-02-23 07:10:27 +0100

    Fixed statusbar stuff

--- app/src/main/java/com/pierrejacquier/todoboard/screens/board/BoardActivity.kt
+++ app/src/main/java/com/pierrejacquier/todoboard/screens/board/BoardActivity.kt
@@ -115,7 +115,7 @@ class BoardActivity : RxBaseActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         headerLayout.setOnClickListener {
-            popToolbar()
+            showSystemUI()
         }
 
         window.decorView.setOnSystemUiVisibilityChangeListener {
@@ -212,16 +212,33 @@ class BoardActivity : RxBaseActivity() {
         }, 2200)
     }
 
-    // TODO: check activity height if hidden
     private fun hideSystemUI() {
+//        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+//                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                or View.SYSTEM_UI_FLAG_FULLSCREEN
+//                or View.SYSTEM_UI_FLAG_IMMERSIVE)
         window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+
+                or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+
                 or View.SYSTEM_UI_FLAG_IMMERSIVE)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
+    }
+
+    private fun showSystemUI() {
+//        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                or View.SYSTEM_UI_FLAG_VISIBLE
+//                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
 
     private fun showBlocksFragments() {
