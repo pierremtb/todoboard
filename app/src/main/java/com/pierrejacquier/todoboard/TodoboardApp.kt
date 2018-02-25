95d19d43f14062ec14b5180fb091c0f50050081f
Pierre Jacquier <pierrejacquier39@gmail.com>
2018-02-25 10:21:25 +0100

    Polished description

--- app/src/main/java/com/pierrejacquier/todoboard/TodoboardApp.kt
+++ app/src/main/java/com/pierrejacquier/todoboard/TodoboardApp.kt
@@ -6,13 +6,14 @@ import com.pierrejacquier.todoboard.di.DaggerTodoboardAppComponent
 import com.pierrejacquier.todoboard.di.TodoboardAppComponent
 import timber.log.Timber
 import android.app.Activity
+import android.support.multidex.MultiDexApplication
 import android.support.v4.app.Fragment
 import com.pierrejacquier.todoboard.commons.TzInfoTypeAdapter
 import paperparcel.Adapter
 import paperparcel.ProcessorConfig
 
 @ProcessorConfig(adapters = arrayOf(Adapter(TzInfoTypeAdapter::class)))
-class TodoboardApp : Application() {
+class TodoboardApp : MultiDexApplication() {
     companion object {
         fun withActivity(activity: Activity) = activity.application as TodoboardApp
         fun withFragment(fragment: Fragment) = fragment.activity?.application as TodoboardApp
